bd0ccfce93b92bf371df757d1904bb72e63b0746
1306112 <1306112@sttgarut.ac.id>
2017-01-28 16:46:01 +0700

    Initial commit

---
+++ app/src/main/java/com/example/japar/uastelepon/uastelepon.java
@@ -0,0 +1,31 @@
+package com.example.japar.uastelepon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class uastelepon extends Activity {
+
+    @Override
+    protected void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.uastelepon);
+        final EditText number=(EditText)findViewById(R.id.editText);
+        Button dial=(Button)findViewById(R.id.button);
+        //ketika menekan tombol call maka akan melakukan dial
+        dial.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                //number = inputan dari editText
+                String toDial="tel:"+number.getText().toString();
+
+                startActivity(new Intent(Intent.ACTION_DIAL,Uri.parse(toDial)));
+            }
+        });
+    }
+}
